12aced77238311b8f33cf9c5f76c9e68f9fc561c
Aurélien Roux <aureroux13@me.com>
2017-01-21 00:33:44 +0000

    created all functions adn started inquirer

--- reddit.js
+++ reddit.js
@@ -1,60 +1,62 @@
 var request = require('request');
+var requestPromise = require("request-promise");
+var inquirer = require('inquirer');
+var colors = require("colors");
 
-/*
-This function should "return" the default homepage posts as an array of objects
-*/
-function getHomepage(callback) {
-  // Load reddit.com/.json and call back with the array of posts
-  // TODO: REPLACE request with requestAsJson!
-  request('https://reddit.com/.json', function(err, res) {
-    if (err) {
-      callback(err);
-    }
-    else {
-      try {
-        var response = JSON.parse(res.body);
-        callback(null, response.data.children); // look at the result and explain why we're returning .data.children
-      }
-      catch(e) {
-        callback(e);
-      }
-    }
-  });
+/*This function should "return" the default homepage posts as an array of objects*/
+function getHomepage() {
+  return requestPromise('https://reddit.com/.json');
 }
 
-/*
-This function should "return" the default homepage posts as an array of objects.
-In contrast to the `getHomepage` function, this one accepts a `sortingMethod` parameter.
-*/
-function getSortedHomepage(sortingMethod, callback) {
-  // Load reddit.com/{sortingMethod}.json and call back with the array of posts
-  // Check if the sorting method is valid based on the various Reddit sorting methods
+/*This function should "return" the default homepage posts as an array of objects.
+In contrast to the `getHomepage` function, this one accepts a `sortingMethod` parameter.*/
+function getSortedHomepage(sortingMethod) {
+  return requestPromise('https://reddit.com/' + sortingMethod + '.json');
 }
 
-/*
-This function should "return" the posts on the front page of a subreddit as an array of objects.
-*/
-function getSubreddit(subreddit, callback) {
-  // Load reddit.com/r/{subreddit}.json and call back with the array of posts
+/*This function should "return" the posts on the front page of a subreddit as an array of objects.*/
+function getSubreddit(subreddit) {
+  return requestPromise('https://reddit.com/r/' + subreddit + '.json');
 }
 
-/*
-This function should "return" the posts on the front page of a subreddit as an array of objects.
-In contrast to the `getSubreddit` function, this one accepts a `sortingMethod` parameter.
-*/
-function getSortedSubreddit(subreddit, sortingMethod, callback) {
-  // Load reddit.com/r/{subreddit}/{sortingMethod}.json and call back with the array of posts
-  // Check if the sorting method is valid based on the various Reddit sorting methods
+/*This function should "return" the posts on the front page of a subreddit as an array of objects.
+In contrast to the `getSubreddit` function, this one accepts a `sortingMethod` parameter.*/
+function getSortedSubreddit(subreddit, sortingMethod) {
+  return requestPromise('https://reddit.com/r/' + subreddit + "/" + sortingMethod +'.json');
 }
 
-/*
-This function should "return" all the popular subreddits
-*/
-function getSubreddits(callback) {
-  // Load reddit.com/subreddits.json and call back with an array of subreddits
+/*This function should "return" all the popular subreddits*/
+function getSubreddits() {
+  return requestPromise('https://reddit.com/subreddits.json');
 }
 
 // Export the API
 module.exports = {
-  // ...
+  getHomepage: getHomepage,
+  getSortedHomepage: getSortedHomepage,
+  getSubreddit: getSubreddit,
+  getSortedSubreddit: getSortedSubreddit,
+  getSubreddits: getSubreddits
 };
+
+//INQUIRER APP ****************************************************
+var menuChoices = [
+  {name: 'Show homepage', value: 'HOMEPAGE'},
+  {name: 'Show subreddit', value: 'SUBREDDIT'},
+  {name: 'List subreddits', value: 'SUBREDDITS'}
+];
+
+inquirer.prompt({
+  type: 'list',
+  name: 'menu',
+  message: 'What do you want to do?',
+  choices: menuChoices
+}).then(
+  function(answers) {
+    if( answers.menu == "HOMEPAGE"){
+      var data = getHomepage();
+      var pdata = JSON.parse(data);
+      console.log(pdata);
+    }
+  }
+);
